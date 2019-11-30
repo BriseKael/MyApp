f8a41ebc4ab3186b01651beb7b6565a8751caacd
Brise.Kael <brise.kael@outlook.com>
2019-11-30 20:41:43 +0800

    [feature/bugfix] F002 [brief description] 增加了一个测试按钮，按下后会打印日志 [user id] Brise.Kael

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.myapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnTestAlpha).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnTestAlpha: {
+                doAlphaTest();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    private void doAlphaTest() {
+        Log.i(TAG, "start to do alpha test");
     }
 }
